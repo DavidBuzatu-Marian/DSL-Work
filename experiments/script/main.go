23a8450c319af91823f1a5fd496959d290cdbc09
davidbuzatu-marian <davidm.buzatu@gmail.com>
2023-01-18 20:15:08 +0100

    Finalized initial go script

--- experiments/script/main.go
+++ experiments/script/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -10,13 +11,16 @@ import (
 )
 
 func main() {
-	app := "kn"
+	app := "sudo"
 	outputFile, err := os.OpenFile("experiment.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer outputFile.Close()
-	for _, name := range os.Args {
+	for idx, name := range os.Args {
+		if idx == 0 {
+			continue
+		}
 		fmt.Println("Running experiment...")
 		podName := strings.Split(name, "-")[1]
 		arg0 := "service"
@@ -24,32 +28,34 @@ func main() {
 		arg2 := name
 		arg3 := "-f"
 		arg4 := "./" + podName + "/" + podName + "_base.yaml"
-		arg5 := "--concurrency-target 1"
+		arg5 := "--concurrency-target=1"
 
 		// Run Pod
 		start := time.Now()
-		cmd := exec.Command(app, arg0, arg1, arg2, arg3, arg4, arg5)
-		elapsed := time.Since(start)
+		cmd := exec.Command(app, "kn", arg0, arg1, arg2, arg3, arg4, arg5)
+		var stderr bytes.Buffer
+		cmd.Stderr = &stderr
 		_, err := cmd.Output()
-
+		elapsed := time.Since(start).Milliseconds()
 		// Stop if any error occurs
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println(err.Error(), stderr.String())
 			return
 		}
 
 		// Write experiment to file
-		experimentOutput := fmt.Sprintf("Run with pod: %s.\t Start container in: %s\n", podName, elapsed)
+		experimentOutput := fmt.Sprintf("Run with pod: %s.\t Start container in: %v(ms)\n", podName, elapsed)
 		if _, err := outputFile.WriteString(experimentOutput); err != nil {
 			log.Println(err)
 			log.Println(experimentOutput)
 		}
 		// Delete all kube resources
 		fmt.Println("Cleaning up...")
-		cmd = exec.Command("kubectl", "delete all", "--all")
+		cmd = exec.Command("sudo", "kubectl", "delete", "all", "--all")
+		cmd.Stderr = &stderr
 		_, err = cmd.Output()
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println(err.Error(), stderr.String())
 			return
 		}
 	}
